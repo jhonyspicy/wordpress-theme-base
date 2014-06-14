8fc30baad696ad124cf19bc949d79d8a42b793c5
jhonyspicy <jhonyspicy@gmail.com>
2014-06-14 12:58:33 +0900

    fixed syntax error

--- index.php
+++ index.php
@@ -6,16 +6,16 @@ if (have_posts()){
 	while(have_posts()){
 		the_post();
 		echo <<<EOTHTML
-		<article id="post-{$with(get_the_ID()}">
+		<article id="post-{$with(get_the_ID())}">
 			{$with(getho('the_title', '<header class="entry-header"><h1 class="entry-title">', '</h1></header>'))}
 			<div class="entry-content">
 				{$with(getho('the_content'))}
 				{$with(getho('wp_link_pages', array(
-					'before'      => '<div class="page-links"><span class="page-links-title">' . __('Pages:') . '</span>',
-					'after'       => '</div>',
-					'link_before' => '<span>',
-					'link_after'  => '</span>',
-				)))}
+			'before'      => '<div class="page-links"><span class="page-links-title">' . __('Pages:') . '</span>',
+			'after'       => '</div>',
+			'link_before' => '<span>',
+			'link_after'  => '</span>',
+		)))}
 			</div>
 		</article>
 EOTHTML;
